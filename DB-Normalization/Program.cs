4e0eba8941d3906b6be8b541284e480e112c6f0e
Ido Tanne <idovader@gmail.com>
2022-02-24 18:08:36 -0500

    Added Final code

--- DB-Normalization/Program.cs
+++ DB-Normalization/Program.cs
@@ -1,10 +1,106 @@
 ﻿using DB_Normalization.DataModels;
-using DB_Normalization.Enums;
+using DB_Normalization.Utilites;
+
+
+//Get File Path
+Console.WriteLine("Please enter the absolute file path of the playlist:\r");
+var filePath = Console.ReadLine();
+var records = CsvRead.FromPath<CsvSong, CsvSongMap>(filePath);
 
 using var myContext = new Context();
+foreach (var record in records)
+{
+    
+    if (!myContext.Songs.Any(s => s.Song_Name == record.Name))//Checks if song name in db
+    {
+        Console.WriteLine($"Song Name: Adding {record.Name} to db");
+        //Check if Album is in db
+        var album = new Albums();
+        if (!myContext.Albums.Any(a => a.Album_Name == record.Album))
+        {
+            Console.WriteLine($"Album Name: Adding {record.Album} to db");
+            album = new Albums()
+            {
+                Album_Name = record.Album,
+                Release_Year = record.Year
+            };
+        }
+        else
+        {
+            album = myContext.Albums.First(a => a.Album_Name == record.Album);
+        }
+        
+        //Check if Artist is in db
+        var artist = new Artists();
+        if (!myContext.Artists.Any(a => a.Artist_Name == record.Artist))
+        {
+            Console.WriteLine($"Artist Name: Adding {record.Artist} to db");
+            artist = new Artists()
+            {
+                Artist_Name = record.Artist
+            };
+        }
+        else
+        {
+            artist = myContext.Artists.First(a => a.Artist_Name == record.Artist);
+        }
+        
+        //Check if Genre is in db
+        var genre = new Genres();
+        if (!myContext.Genres.Any(g => g.Genre_Name == record.Genre))
+        {
+            Console.WriteLine($"Genre Name: Adding {record.Genre} to db");
+            genre = new Genres()
+            {
+                Genre_Name = record.Genre
+            };
+        }
+        else
+        {
+            genre = myContext.Genres.First(g => g.Genre_Name == record.Genre);
+        }
+        
+        var song = new Songs()
+        {
+            Album_of_Song = album,
+            Song_Artist = artist,
+            Song_Genre = genre,
+            Song_Name = record.Name
+        };
+        
+        myContext.Add(song);
+        myContext.SaveChanges();
+        
+    }
+    else
+    {
+        Console.WriteLine($"{record.Name} is already in db");
+    }
+    Console.WriteLine("-------------------------------------------");
+}
 
-var myClasses = myContext.Class.Where(c => c.MaxClassSize > 0)
-    .ToList();
-myContext.SaveChanges();
 
-Console.Write("");
+
+
+
+/* OLD EXAMPLE
+ 
+ using var myContext = new Context();
+myContext.Add(new Songs()
+{
+    Album_of_Song = new Albums()
+    {
+        Album_Name = "Black Parade",
+        Release_Year = 2001
+    },
+    Song_Artist = new Artists()
+    {
+        Artist_Name = "MCR"
+    },
+    Song_Genre = new Genres()
+    {
+        Genre_Name = "Punk"
+    },
+    Song_Name = "welcome to the black parade"
+});
+myContext.SaveChanges();*/
